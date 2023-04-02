7e9c4924a775d4eb2f7e7c7f1f7b50962e6660ec
by-westik <nastya.derevyanko.2020@mail.ru>
2023-04-02 13:53:04 +0300

    Add JWT verifier and validate

--- HotKitchen/task/src/Application.kt
+++ HotKitchen/task/src/Application.kt
@@ -1,20 +1,40 @@
 package hotkitchen
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import hotkitchen.database.configureDatabase
 import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.auth.jwt.*
 import io.ktor.features.*
 import io.ktor.serialization.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-/*
+
+
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
-*/
+/*
 fun main() {
     embeddedServer(Netty, port = 28852, host = "0.0.0.0", module = Application::module).start(wait = true)
-}
+}*/
 
 fun Application.module(testing: Boolean = false) {
     install(ContentNegotiation) { json() }
+    install(Authentication) {
+        jwt {
+            val secret = environment.config.property("jwt.secret").getString()
+            verifier(JWT.require(Algorithm.HMAC256(secret)).build())
+            validate { credential ->
+                if (credential.payload.getClaim("userType").asString() != "" &&
+                    credential.payload.getClaim("email").asString() != "") {
+                        JWTPrincipal(credential.payload)
+                } else {
+                    null
+                }
+            }
+            // Configure jwt authentication
+        }
+    }
     configureRouting()
     configureDatabase()
 }
--- HotKitchen/task/src/Routing.kt
+++ HotKitchen/task/src/Routing.kt
@@ -5,7 +5,6 @@ import hotkitchen.data.SignIn
 import hotkitchen.data.User
 import hotkitchen.database.DatabaseController
 import io.ktor.application.*
-import io.ktor.auth.*
 import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
@@ -13,36 +12,14 @@ import io.ktor.routing.*
 
 fun Application.configureRouting() {
     routing {
-        authenticate("myFormAuth") {
-            authenticate("myFormAuth2") {
-                post("/page") {
-                    call.respondText("Protected content! Name: ${call.principal<UserIdPrincipal>()?.name}")
-                }
-            }
-        }
-        get("/page") {
-            call.respondRedirect("/auth")
-        }
-        get("/auth") {
-            val formHtml = """
-    <form action="/page" method="post">
-        <input type="text" name="username">
-        <input type="password" name="password">
-        <input type="submit" value="Auth">
-    </form>
-""".trimIndent()
-            call.respondText(formHtml, ContentType.Text.Html)
-        }
         get("/main"){
             call.respondText("Hello World!")
         }
-    }
-/*
         post("/signup") {
             val user = call.receive<User>()
             if (DatabaseController.getUserByEmail(user.email) == null) {
                 DatabaseController.saveUser(user)
-                call.respond(HttpStatusCode.OK, ResponseStatus("Signed In"))
+                call.respond(HttpStatusCode.OK, ResponseStatus("Signed Up"))
             } else {
                 call.respond(HttpStatusCode.Forbidden, ResponseStatus("Registration failed"))
             }
@@ -52,10 +29,10 @@ fun Application.configureRouting() {
             val signInUser = call.receive<SignIn>()
             val user = DatabaseController.getUserByEmail(signInUser.email)
             if (user != null && user.password == signInUser.password) {
-                call.respond(HttpStatusCode.OK, ResponseStatus("Signed Up"))
+                call.respond(HttpStatusCode.OK, ResponseStatus("Signed In"))
             } else {
                 call.respond(HttpStatusCode.Forbidden, ResponseStatus("Authorization failed"))
             }
         }
-    }*/
+    }
 }
